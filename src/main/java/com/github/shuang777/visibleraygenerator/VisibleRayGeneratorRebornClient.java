8ee6a3fa8726eac0a0f186b5989e3ea515a6664b
777shuang <shuang777@outlook.jp>
2026-06-14 18:52:33 +0900

    整形

--- src/main/java/com/github/shuang777/visibleraygenerator/VisibleRayGeneratorRebornClient.java
+++ src/main/java/com/github/shuang777/visibleraygenerator/VisibleRayGeneratorRebornClient.java
@@ -15,17 +15,17 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
 @EventBusSubscriber(modid = VisibleRayGeneratorReborn.MODID, value = Dist.CLIENT)
 public class VisibleRayGeneratorRebornClient {
-    public VisibleRayGeneratorRebornClient(ModContainer container) {
-        // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
-        // Do not forget to add translations for your config options to the en_us.json file.
-        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
-    }
+  public VisibleRayGeneratorRebornClient(ModContainer container) {
+    // Allows NeoForge to create a config screen for this mod's configs.
+    // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+    // Do not forget to add translations for your config options to the en_us.json file.
+    container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+  }
 
-    @SubscribeEvent
-    static void onClientSetup(FMLClientSetupEvent event) {
-        // Some client setup code
-        VisibleRayGeneratorReborn.LOGGER.info("HELLO FROM CLIENT SETUP");
-        VisibleRayGeneratorReborn.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-    }
+  @SubscribeEvent
+  static void onClientSetup(FMLClientSetupEvent event) {
+    // Some client setup code
+    VisibleRayGeneratorReborn.LOGGER.info("HELLO FROM CLIENT SETUP");
+    VisibleRayGeneratorReborn.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+  }
 }
